527417a94962eaa62a5577c2c162201dec740056
ninteappantest <82845096+niteappantest@users.noreply.github.com>
2021-09-19 16:28:57 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -5,11 +5,8 @@ WhatsAsena - Yusuf Usta
 Developer & Co-Founder - Phaticusthiccy
 */
 
-const Asena = require("../Utilis/events");
+const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const { checkImAdmin } = require("../Utilis/Misc");
-const { warn, getEachWarn } = require("../Utilis/warn");
-const { getBuffer } = require("../Utilis/download");
 const {spawnSync} = require('child_process');
 const Config = require('../config');
 const chalk = require('chalk');
@@ -17,7 +14,6 @@ const axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
-let wk = true;
 
 
 if (Config.WORKTYPE == 'private') {
@@ -39,7 +35,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-   Asena.addCommand({pattern: 'alive', fromMe: wk, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+   Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
         
         let pp
         try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
@@ -54,47 +50,11 @@ else if (Config.WORKTYPE == 'public') {
         );
     }));
     
-    Asena.addCommand({pattern: 'psysd', fromMe: wk, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({pattern: 'psysd', fromMe: true, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
 
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
             '```' + child + '```', MessageType.text
         );
     }));
-
-    Asena.addCommand(
-  { pattern: "warn ?(.*)", fromMe: wk, desc: "To warn", onlyGroup: true },
-  async (message, match) => {
-    if (match == 'list') {
-      let msg = '';
-      let users = await getEachWarn()
-      if (!users) return await message.sendMessage('*Nothing to Display*')
-      users.forEach(jid => {
-        let { user, count, reason } = jid
-        msg += Lang.WARN_MSG.format(user.split('@')[0], count, Config.WARN_COUNT - count, reason)
-      })
-      return await message.sendMessage(msg)
-    }
-    if (!message.reply_message && !message.mention)
-      return await message.sendMessage(Lang.NEED_USER);
-    let quoted = !message.reply_message ? undefined : message.quoted;
-    let { user, count, reason } = await warn(message, match);
-    if (reason == "reset") {
-      return await message.sendMessage(Lang.RESET.format(user.split("@")[0], Config.WARN_COUNT - count),
-        { quoted, contextInfo: { mentionedJid: [user] } }
-      );
-    }
-    if (count >= Config.WARN_COUNT) {
-      let participants = await message.groupMetadata(message.jid);
-      let im = await checkImAdmin(participants, message.client.user.jid);
-      if (!im) return await message.sendMessage(Lang.IAADMIN);
-      let us = await checkImAdmin(participants, user);
-      if (us) return await message.sendMessage(Lang.ISADMIN);
-      await message.sendMessage(Config.WARN_MSG, { quoted });
-      return await message.groupRemove(message.jid, user);
-    }
-    return await message.sendMessage(Lang.WARNING.format(user.split("@")[0], reason, Config.WARN_COUNT - count),
-      { quoted, contextInfo: { mentionedJid: [user] } }
-    );
-  }
-);
+}
